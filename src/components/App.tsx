c706e5f679ecd907efeda145f3833714dc8f4ec4
dieegobs <diegohevant@gmail.com>
2023-02-08 23:50:17 -0300

    Usando map para fazer a listagem

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,22 @@ import React from 'react'
 export const App = () => {
 
     let list = [
-
+            'Diego',
+            'João',
+            'Maria'
     ];
 
+    
+
   return (
-    <div>App</div>
-  )
+    <div>
+        <h2>Lista de presença</h2>
+        <ul>
+            {list.map((item) => (
+                <li>{item}</li>
+            ))}
+        </ul>
+
+    </div>
+  );
 }
